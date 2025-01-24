217aec843c6bfb75550ad4aefba10f42829b8dde
Dairon Luis <daironluis00@gmail.com>
2025-01-24 12:01:15 -0500

    Add some structures

---
+++ Assets/Hierarchy Label Tool/Runtime/HierarchyLabelData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HierarchyLabelTool
+{
+    [CreateAssetMenu(menuName = "Tooling/Hierarchy Label Data")]
+    public class HierarchyLabelData : ScriptableObject
+    {
+        public string text = "Label";
+        [ColorUsage(false, false)]
+        public Color textColor = Color.black;
+        [ColorUsage(false, false)]
+        public Color backgroundColor = Color.cyan;
+        [TextArea, Tooltip("Details about this label's purpose and use.")]
+        public string labelDescription;
+    }
+}
