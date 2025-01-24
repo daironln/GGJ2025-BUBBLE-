96919cf8b6e56b33e44c35c28beb44309c6e6ef5
Dairon Luis <daironluis00@gmail.com>
2025-01-24 13:44:34 -0500

    Some sanges

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,8 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+
+[Serializable]
+public enum GameState{
+    None,
+    InMainMenu,
+    InPause,
+    InPlay,
+    InQuit,
+    InGameOver
+}
 public class GameManager : PersistentSingleton<GameManager>
 {
-    
+    public GameState gameState;
+
+
+    private void Start()
+    {
+        gameState = GameState.None;
+    }   
+
+
+    public void Pause(bool paused)
+    {
+        if(paused)
+        {
+            gameState = GameState.InPlay;
+        }
+        else
+        {
+            gameState = GameState.InPause;
+
+            Time.timeScale = 0;
+        }
+    }
 }
