b462c9426cc55b092a5a7326b0c5c3694a5f722e
Dairon Luis <daironluis00@gmail.com>
2025-01-24 09:43:19 -0500

    Add some scripts

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
+[Serializable]
+public enum GameState
+{
+    InMainMenu,
+    InPlay,
+    InPause,
+    InQuit,
+    InGameOver
+}
 
 public class GameManager : PersistentSingleton<GameManager>
 {
-    
+    public GameState State;
+
+    private void Start()
+    {
+        State = GameState.InMainMenu;
+    }
+
+    public void Pause(bool pause)
+    {
+        if(pause)
+            State = GameState.InPlay;
+
+        else
+            State = GameState.InPause;
+
+        Time.timeScale = 0;
+    }
 }
