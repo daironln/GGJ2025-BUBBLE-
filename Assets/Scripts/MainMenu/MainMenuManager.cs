ddfce62dd1c6563fb06fa12014adc46bdf95dec0
Dairon Luis <daironluis00@gmail.com>
2025-01-25 00:22:15 -0500

    Update

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{   
+    private void StartGame()
+    {
+
+    }
+
+    private void ContinueGame()
+    {
+
+    }
+
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void AboutUs()
+    {
+
+    }
+}
